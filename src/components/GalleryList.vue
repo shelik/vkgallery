42a5a15e664b322fed9a0abe1af055e2dcbd12b5
shelik <shelkoplias@gmail.com>
2021-12-21 11:29:32 +0200

    fix: gallerylist component

---
+++ src/components/GalleryList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GalleryList",
+  props: {
+    galleries: [],
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,8 @@ module.exports = {
     devServer: {
         proxy: {
             '/api/': {
-                target: 'http://localhost:8888',
+                // target: 'http://localhost:8888',
+                target: 'http://138.197.5.8:8888',
                 ws: true,   
                 changeOrigin: true,
             },
