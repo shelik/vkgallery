97ef7c1628fadde6bb3075427b88a6bb1a7bd555
shelik <shelkoplias@gmail.com>
2021-12-23 21:06:42 +0200

    fix: gitignore

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  devServer: {
+    proxy: {
+      "/api/": {
+        // target: 'http://localhost:8888',
+        target: "http://138.197.5.8:8888",
+        ws: true,
+        changeOrigin: true,
+      },
+    },
+  },
+
+  transpileDependencies: ["vuetify"],
+};
